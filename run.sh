2fe78f2c3dbff018d3c17d59a1405e3efd148fd5
mblack <martin@parkopedia.com>
2021-08-24 09:10:25 +0100

    Simplify scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-TAG=${TAG:-latest}
-DEFAULT_IMAGE=$"rtree-bench":$TAG
-IMAGE=${IMAGE:-$DEFAULT_IMAGE}
-
-# --net=host is required to reach the prediction-api
-docker run -ti $IMAGE "$@"
+docker run -ti "rtree-bench:latest" "$@"
